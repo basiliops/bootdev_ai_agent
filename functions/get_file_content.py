5bba8b5c317d31d415f81c62938301b5f30ba757
basiliops <dennis.basilio@pm.me>
2025-08-17 09:40:24 -0700

    Create write file functionality

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -2,14 +2,14 @@ import os
 from functions.config import *
 
 def get_file_content(working_directory, file_path):
-    full_path = os.path.join(working_directory, file_path)
-    if not os.path.abspath(full_path).startswith(os.path.abspath(working_directory)):
+    abs_full_path = os.path.abspath(os.path.join(working_directory, file_path))
+    if not abs_full_path.startswith(os.path.abspath(working_directory)):
          return f'Error: Cannot list "{file_path}" as it is outside the permitted working directory'
-    if not os.path.isfile(os.path.abspath(full_path)):
+    if not os.path.isfile(abs_full_path):
          return f'Error: File not found or is not a regular file: "{file_path}"'
 
     try:
-        with open(os.path.abspath(full_path), "r") as f:
+        with open(abs_full_path, "r") as f:
             file_content_string = f.read(MAX_CHARS)
             if len(f.read()) > MAX_CHARS:
                  file_content_string += f'[...File "{file_path}" truncated at 10000 characters]'
