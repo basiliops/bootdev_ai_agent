d17cc000dc787efff07f12c3bc5cbc6d6f5d4af2
basiliops <dennis.basilio@pm.me>
2025-08-19 20:48:47 -0700

    Updated prompt and added first function call

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,5 @@
 import os
+from google.genai import types
 
 def get_files_info(working_directory, directory="."):
     abs_full_path = os.path.abspath(os.path.join(working_directory, directory))
@@ -14,3 +15,17 @@ def get_files_info(working_directory, directory="."):
         return "\n".join(item_list)
     except Exception as e:
         return f'Error listing files: {e}'
+    
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
